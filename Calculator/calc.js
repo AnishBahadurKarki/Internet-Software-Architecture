d4d54c29d44752f8acdd0b8430a60b01569f1f18
AnishBahadurKarki <np03cs4a220011@heraldcollege.edu.np>
2023-03-29 21:35:32 +0545

    Just Updated File of Calculator Js

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,12 +1,19 @@
-const display = document.getElementById("display");
+//This line declares a constant display and assigns it to the DOM element with the id of "display".
+const display = document.getElementById("display"); 
+//These lines declare variables num1, num2, operation, and result and initialize them to null.
 let num1 = null;
 let num2 = null;
 let operation = null;
 let result = null;
 
+//This line declares a function numberClick that takes an event object as a parameter.
+
 function numberClick(event) {
+
+  //This line declares a constant numString and assigns it to the value of the target element that triggered the event.
   const numString = event.target.value;
 
+  //These lines check if the operation is null or not, and if it is, num1 is assigned numString if it is null or concatenated with numString if it is not. Otherwise, num2 is assigned numString if it is null or concatenated with numString if it is not.
   if (!operation) {
     num1 = num1 ? num1 + numString : numString;
   } else {
@@ -36,6 +43,7 @@ function clearClick() {
   result = null;
   display.value = "";
 }
+//These lines call calculateResult with num1, num2, and operation as arguments, and assign the returned value to result. Then, the value of the display element is set to result, num1 is assigned result,
 
 function equalsClick() {
   result = calculateResult(num1, num2, operation);
